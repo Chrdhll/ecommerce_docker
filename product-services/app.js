b93e29c1236dc0d28614f7a5639e0fe326b79bf9
Fadhil <m.fadil3434@gmail.com>
2025-11-08 09:31:04 +0700

    update cart

--- product-services/app.js
+++ product-services/app.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
 
 //dummy data produk
 const products = [
-    { id: 1, name: 'mio karbu', price: 100, description: 'Description for Product A' },
+    { id: 1, name: 'Laptop lenovo', price: 100, description: 'Description for Product A' },
     { id: 2, name: 'PS 5', price: 150, description: 'Description for Product B' },
     { id: 3, name: 'Xbox', price: 200, description: 'Description for Product C' },
+    { id: 4, name: 'Smart TV', price: 250, description: 'Description for Product D' },
+    { id: 5, name: 'Kulkas', price: 300, description: 'Description for Product E' },
+    { id: 6, name: 'Gitar Listrik', price: 350, description: 'Description for Product F' },
+    { id: 7, name: 'Drone', price: 400, description: 'Description for Product G' },
+    { id: 8, name: 'Bass Guitar', price: 450, description: 'Description for Product H' },
+    { id: 9, name: 'Electric Violin', price: 500, description: 'Description for Product I' },
+    { id: 10, name: 'Piano', price: 550, description: 'Description for Product J' },
 ];
 
+app.use(cors());
 //endpoint untuk mendapatkan daftar produk
 app.get('/products', (req, res) => {
     res.json(products);
